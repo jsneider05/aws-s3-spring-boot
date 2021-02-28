515c4a936c89b5c3c5f2a9af409e7c584c2a7923
joan.garcia <joan.garcia@ceiba.com.co>
2021-02-28 18:26:36 -0500

    DEVUS: AWS-S3: Demo project for upload images in AWS S3 (First commit)

---
+++ src/main/java/com/practice/awss3springboot/AwsS3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.practice.awss3springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3SpringBootApplication.class, args);
+	}
+
+}
