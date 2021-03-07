3d7405f74be699d13de3d8e1559abe32a92ad5fc
joan.garcia <joan.garcia@ceiba.com.co>
2021-03-07 18:13:42 -0500

    DEVUS: AWS-S3: Demo project for upload images in AWS S3 (UI Logic to send files to backend, add path, consumes and produces values to post controller)

--- src/main/java/com/practice/awss3/infrastructure/controller/UserProfileController.java
+++ src/main/java/com/practice/awss3/infrastructure/controller/UserProfileController.java
@@ -5,9 +5,11 @@ import com.practice.awss3.domain.model.UserProfile;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,11 @@ public class UserProfileController {
     return this.obtainUserProfileHandler.getUserProfile();
   }
 
+  @PostMapping(
+      path = "{userProfileId}/image/upload",
+      consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
   public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
       @RequestParam("file") MultipartFile file) {
 
